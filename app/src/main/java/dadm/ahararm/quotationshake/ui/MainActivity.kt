89d829d737b7242872c064e7dbd1736c68e4d0b1
Abel Haro <ahararm@upv.es>
2025-02-20 16:15:44 +0100

    Práctica 01, Ejercicio 06 terminado

--- app/src/main/java/dadm/ahararm/quotationshake/ui/MainActivity.kt
+++ app/src/main/java/dadm/ahararm/quotationshake/ui/MainActivity.kt
@@ -1,10 +1,12 @@
 package dadm.ahararm.quotationshake.ui
 
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -28,6 +30,54 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         binding.toolbar.setupWithNavController(navController, appBarConfig)
+
+        val navigationBar = binding.bnvMain
+
+        // Aplicar los insets a la barra de navegación
+        ViewCompat.setOnApplyWindowInsetsListener(navigationBar) { view, insets ->
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.displayCutout() or
+                        WindowInsetsCompat.Type.systemBars()
+            )
+            view.updatePadding(
+                left = bars.left,
+                top = 0,
+                right = 0,
+                bottom = bars.bottom
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+
+        val fragmentContainerView = binding.fcvNavHostFragment
+        ViewCompat.setOnApplyWindowInsetsListener(fragmentContainerView) { view, insets ->
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.displayCutout() or
+                        WindowInsetsCompat.Type.systemBars()
+            )
+            view.updatePadding(
+                left = 0,
+                top = 0,
+                right = bars.right,
+                bottom = if (resources.configuration.orientation == ORIENTATION_LANDSCAPE) bars.bottom else 0
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+
+        val appBarLayout = binding.ablMain
+
+        ViewCompat.setOnApplyWindowInsetsListener(appBarLayout) { view, insets ->
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.displayCutout() or
+                        WindowInsetsCompat.Type.systemBars()
+            )
+            view.updatePadding(
+                left = bars.left,
+                top = bars.top,
+                right = 0,
+                bottom = 0
+            )
+            WindowInsetsCompat.CONSUMED
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
