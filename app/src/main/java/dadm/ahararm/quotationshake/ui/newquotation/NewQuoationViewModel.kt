b43a36d5b45debe3b4f7cf3e0ef96955a767e55b
Abel Haro Armero <ahararm@upv.edu.es>
2025-02-24 17:16:26 +0100

    Práctica 01, Ejercicio 12 terminado

--- app/src/main/java/dadm/ahararm/quotationshake/ui/newquotation/NewQuoationViewModel.kt
+++ app/src/main/java/dadm/ahararm/quotationshake/ui/newquotation/NewQuoationViewModel.kt
@@ -1,9 +1,11 @@
 package dadm.ahararm.quotationshake.ui.newquotation
 
 import androidx.lifecycle.ViewModel
+import dadm.ahararm.quotationshake.domain.model.Quotation
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class NewQuotationViewModel : ViewModel() {
 
@@ -13,7 +15,27 @@ class NewQuotationViewModel : ViewModel() {
     // Propiedad pública de solo lectura como StateFlow
     val userName: StateFlow<String> = _userName.asStateFlow()
 
+    // Estado interno mutable
+    private val _quotation: MutableStateFlow<Quotation?> = MutableStateFlow(null)
+    val quotation: StateFlow<Quotation?> = _quotation.asStateFlow()
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
     private fun getUserName(): String {
         return setOf("Alice", "Bob", "Charlie", "David", "Emma", "").random()
+    }
+
+    fun getNewQuotation() {
+        _isLoading.value = true
+        val num = (0..99).random()
+        _quotation.update {
+            Quotation(
+                id = "$num",
+                text = "Quotation text #$num",
+                author = "Author #$num"
+            )
+        }
+        _isLoading.value = false
     }
 }
--- app/src/main/java/dadm/ahararm/quotationshake/ui/newquotation/NewQuotationFragment.kt
+++ app/src/main/java/dadm/ahararm/quotationshake/ui/newquotation/NewQuotationFragment.kt
@@ -2,6 +2,7 @@ package dadm.ahararm.quotationshake.ui.newquotation
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -24,13 +25,31 @@ class NewQuotationFragment : Fragment(R.layout.fragment_new_quotation) {
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.userName.collect { userName ->
-                    binding.tvWelcomeMessage.text = getString(
-                        R.string.welcome_message,
-                        userName.ifEmpty { getString(R.string.anonymous) }
-                    )
+
+                binding.tvAuthor.isVisible = false
+
+                viewModel.quotation.collect { quotation ->
+                    binding.tvWelcomeMessage.isVisible = quotation == null
+                    binding.tvQuote.text = quotation?.text ?: ""
+                    binding.tvAuthor.text = quotation?.author.takeIf { it?.isNotEmpty() == true }
+                        ?: getString(R.string.anonymous)
+                    binding.tvAuthor.isVisible = quotation != null
                 }
             }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.isLoading.collect { isLoading ->
+                        binding.srlMain.isRefreshing = isLoading
+                    }
+                }
+            }
+        }
+
+        binding.srlMain.setOnRefreshListener {
+            viewModel.getNewQuotation()
         }
     }
 
