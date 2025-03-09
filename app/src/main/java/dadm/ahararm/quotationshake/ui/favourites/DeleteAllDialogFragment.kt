0d1920861bff47cafbe689e9426bf4eda5fc9e18
Abel Haro Armero <ahararm@upv.edu.es>
2025-03-09 18:49:35 +0100

    Ajustes layouts horizontales

--- app/src/main/java/dadm/ahararm/quotationshake/ui/favourites/DeleteAllDialogFragment.kt
+++ app/src/main/java/dadm/ahararm/quotationshake/ui/favourites/DeleteAllDialogFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
+import dadm.ahararm.quotationshake.R
 
 class DeleteAllDialogFragment : DialogFragment() {
 
@@ -12,12 +13,12 @@ class DeleteAllDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(requireContext())
-            .setTitle("Delete all favourite quotations")
-            .setMessage("Do you really want to delete all your quotations?")
-            .setPositiveButton("Yes") { _, _ ->
+            .setTitle(getString(R.string.confirm_delete_all_quotes_title))
+            .setMessage(R.string.confirm_delete_all_quotes_text)
+            .setPositiveButton(getString(R.string.confirm_delete_all_quotes_positive_button)) { _, _ ->
                 viewModel.deleteAllDialogFragment()
             }
-            .setNegativeButton("No") { dialog, _ ->
+            .setNegativeButton(R.string.confirm_delete_all_quotes_negative_button) { dialog, _ ->
                 dialog.dismiss()
             }
             .create()
